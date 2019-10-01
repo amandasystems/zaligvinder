2f6e0b74b6ec9be27272e181a8ae90f27b6a46fe
Mitja Kulczynski <mku@informatik.uni-kiel.de>
2019-10-01 17:30:58 +0200

    smt files

--- track04/__init__.py
+++ track04/__init__.py
@@ -2,9 +2,13 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 filest = []
+filesteq = []
 for root, dirs, files in os.walk(dir_path, topdown=False):
     for name in files:
-        if name.endswith ("eq"):
+        if name.endswith (".smt"):
             filest.append(os.path.join (root,name))
-
+        if name.endswith (".eq"):
+            filesteq.append(os.path.join (root,name))
+        
 trackdata = ("track4",filest)
+trackdataeq = ("track4",filesteq)

ecc4606d2f4034cd1604cbbbb191f82c3feb7e41
Mitja Kulczynski <mku@informatik.uni-kiel.de>
2019-12-04 10:15:34 +0100

    ...

---
+++ overlaps_small/__init__.py
@@ -0,0 +1,14 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+filest = []
+filesteq = []
+for root, dirs, files in os.walk(dir_path, topdown=False):
+    for name in files:
+        if name.endswith (".smt20") or name.endswith (".smt25") or name.endswith (".smt2") or name.endswith (".smt"):
+            filest.append(os.path.join (root,name))
+        if name.endswith (".eq"):
+            filesteq.append(os.path.join (root,name))
+
+trackdata = ("z3-overlaps-small",filest)
+trackdataeq = ("z3-overlaps-small",filesteq)

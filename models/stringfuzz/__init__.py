f04d9e786016dff21f5d82a59ff3b738d371be18
Mitja Kulczynski <mku@calhoun.local>
2020-01-09 13:01:48 +0100

    stupid me

--- models/stringfuzz/__init__.py
+++ models/stringfuzz/__init__.py
@@ -1,18 +1,42 @@
 
 
 def getTrackData (bname = None):
-    import models.woorpje.track01
-    import models.woorpje.track02
-    import models.woorpje.track03
-    import models.woorpje.track04
-    import models.woorpje.track05
+    import models.stringfuzz.concats_small
+    import models.stringfuzz.many_regexes
+    import models.stringfuzz.regex_lengths
+    import models.stringfuzz.concats_balanced
+    import models.stringfuzz.different_prefix
+    import models.stringfuzz.overlaps_big
+    import models.stringfuzz.regex_pair
+    import models.stringfuzz.concats_big
+    import models.stringfuzz.lengths_concats
+    import models.stringfuzz.overlaps_small
+    import models.stringfuzz.regex_small
+    import models.stringfuzz.concats_extracts_big
+    import models.stringfuzz.lengths_long
+    import models.stringfuzz.regex_big
+    import models.stringfuzz.concats_extracts_small
+    import models.stringfuzz.lengths_short
+    import models.stringfuzz.regex_deep
     res = []
-    for k in [models.woorpje.track01,
-              models.woorpje.track02,
-              models.woorpje.track03,
-              models.woorpje.track04,
-              models.woorpje.track05]:
-        res = res+k.getTrackData(bname or "Woorpje")
+    for k in [models.stringfuzz.concats_small
+              models.stringfuzz.many_regexes
+              models.stringfuzz.regex_lengths
+              models.stringfuzz.concats_balanced
+              models.stringfuzz.different_prefix
+              models.stringfuzz.overlaps_big
+              models.stringfuzz.regex_pair
+              models.stringfuzz.concats_big
+              models.stringfuzz.lengths_concats
+              models.stringfuzz.overlaps_small
+              models.stringfuzz.regex_small
+              models.stringfuzz.concats_extracts_big
+              models.stringfuzz.lengths_long
+              models.stringfuzz.regex_big
+              models.stringfuzz.concats_extracts_small
+              models.stringfuzz.lengths_short
+              models.stringfuzz.regex_deep]:
+        res = res+k.getTrackData(bname or "StringFuzz")
     return res
               
 

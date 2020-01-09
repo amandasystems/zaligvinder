77737fe8e2f09acdb7929e27644cf54e5a32b620
Mitja Kulczynski <mku@calhoun.local>
2020-01-09 13:02:57 +0100

    commas

--- models/stringfuzz/__init__.py
+++ models/stringfuzz/__init__.py
@@ -19,22 +19,22 @@ def getTrackData (bname = None):
     import models.stringfuzz.lengths_short
     import models.stringfuzz.regex_deep
     res = []
-    for k in [models.stringfuzz.concats_small
-              models.stringfuzz.many_regexes
-              models.stringfuzz.regex_lengths
-              models.stringfuzz.concats_balanced
-              models.stringfuzz.different_prefix
-              models.stringfuzz.overlaps_big
-              models.stringfuzz.regex_pair
-              models.stringfuzz.concats_big
-              models.stringfuzz.lengths_concats
-              models.stringfuzz.overlaps_small
-              models.stringfuzz.regex_small
-              models.stringfuzz.concats_extracts_big
-              models.stringfuzz.lengths_long
-              models.stringfuzz.regex_big
-              models.stringfuzz.concats_extracts_small
-              models.stringfuzz.lengths_short
+    for k in [models.stringfuzz.concats_small,
+              models.stringfuzz.many_regexes,
+              models.stringfuzz.regex_lengths,
+              models.stringfuzz.concats_balanced,
+              models.stringfuzz.different_prefix,
+              models.stringfuzz.overlaps_big,
+              models.stringfuzz.regex_pair,
+              models.stringfuzz.concats_big,
+              models.stringfuzz.lengths_concats,
+              models.stringfuzz.overlaps_small,
+              models.stringfuzz.regex_small,
+              models.stringfuzz.concats_extracts_big,
+              models.stringfuzz.lengths_long,
+              models.stringfuzz.regex_big,
+              models.stringfuzz.concats_extracts_small,
+              models.stringfuzz.lengths_short,
               models.stringfuzz.regex_deep]:
         res = res+k.getTrackData(bname or "StringFuzz")
     return res

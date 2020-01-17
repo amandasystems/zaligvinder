971db23a510ae1c4de5381d141b583c8976f3aa3
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2020-01-17 20:46:08 +0100

    hacked png support in there

--- webserver/controllers/filecontrol.py
+++ webserver/controllers/filecontrol.py
@@ -1,4 +1,5 @@
 import webserver.views.TextView
+import webserver.views.PNGView
 import webserver.files
 
 class FileControl:
@@ -6,7 +7,13 @@ class FileControl:
         self._locator = webserver.files.FileLocator ()
 
     def findFile (self,params):
-        f =  self._locator.findFile (params["path"])
-        if f:
-            return webserver.views.TextView.TextView (f.read())
+        path = params["path"]
+        if path.endswith (".png"):
+            f =  self._locator.findFileBinary (params["path"])
+            if f:
+                return webserver.views.PNGView.PNGView (f)
+        else:
+            f =  self._locator.findFile (params["path"])
+            if f:
+                return webserver.views.TextView.TextView (f.read())
         return webserver.views.TextView.ErrorText ("File Not available")
